262a319cf937bbadb959d3e2797b74c6a8349fb7
YasmineAfifi <ynremotecoders@gmail.com>
2024-02-02 16:07:47 -0500

    login

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -28,17 +28,35 @@ def contact(request):
  return render (request,'djangoapp/contact.html')
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
-# ...
-
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['psw']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/djangoapp/about')
+        else:
+            return render(request,'djangoapp/login.html')
+    else:
+        return render(request, 'djangoapp/login.html')
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
-
+def logout_request(request):
+    logout(request)
+    return redirect('/djangoapp/login')
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
-# ...
-
+def registration_request(request):
+    if request.method == 'GET':
+        return render(request, 'djangoapp/registration.html')
+    elif request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['psw']
+        first_name = request.POST['firstname']
+        last_name = request.POST['lastname']
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name,
+                                            password=password)
+        return render(request, 'djangoapp/login.html')
+    return render(request, 'djangoapp/registration.html')
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
     context = {}
